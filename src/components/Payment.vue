e6046e1cb693d8b6d6cfa631f57703483776cb74
Yan <crowbeat.47@gmail.com>
2025-07-21 17:07:18 +0500

    feat: add star select and change feature

---
+++ src/components/Payment.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div class="w-[23.4375rem] z-50">
+    <div class="py-5 flex justify-center">Some</div>
+  </div>
+</template>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Payment from './Payment.vue'
+import Review from './Review.vue'
+</script>
+<template>
+  <div
+    class="bg-black/20 fixed left-0 top-0 flex justify-center items-center w-full flex-col h-full"
+  >
+    <Review />
+    <Payment />
+  </div>
+</template>
